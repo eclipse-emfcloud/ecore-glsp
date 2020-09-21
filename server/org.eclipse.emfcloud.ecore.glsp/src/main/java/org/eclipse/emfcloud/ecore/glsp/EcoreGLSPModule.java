a9b5b6b181c47d1c8fba2c50babffe3b604e84c1
Camille Letavernier <cletavernier@eclipsesource.com>
2020-09-21 11:31:18 +0200

    #42: Fix label auto-completion
    - Add missing Action declaration on the server
    - Fix the layout of the text-edit control on the client
    
    Signed-off-by: Camille Letavernier <cletavernier@eclipsesource.com>

--- server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/EcoreGLSPModule.java
+++ server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/EcoreGLSPModule.java
@@ -10,6 +10,7 @@
  ********************************************************************************/
 package org.eclipse.emfcloud.ecore.glsp;
 
+import org.eclipse.emfcloud.ecore.glsp.actions.ReturnAttributeTypesAction;
 import org.eclipse.emfcloud.ecore.glsp.handler.EcoreComputedBoundsActionHandler;
 import org.eclipse.emfcloud.ecore.glsp.handler.EcoreGetAttributeTypesActionHandler;
 import org.eclipse.emfcloud.ecore.glsp.handler.EcoreOperationActionHandler;
@@ -26,6 +27,7 @@ import org.eclipse.emfcloud.ecore.glsp.operationhandler.EcoreDeleteOperationHand
 import org.eclipse.emfcloud.ecore.glsp.operationhandler.EcoreLabelEditOperationHandler;
 import org.eclipse.emfcloud.ecore.glsp.palette.EcoreToolPaletteItemProvider;
 import org.eclipse.emfcloud.ecore.glsp.registry.EcoreDIOperationHandlerRegistry;
+import org.eclipse.glsp.api.action.Action;
 import org.eclipse.glsp.api.configuration.ServerConfiguration;
 import org.eclipse.glsp.api.diagram.DiagramConfiguration;
 import org.eclipse.glsp.api.factory.ModelFactory;
@@ -69,6 +71,12 @@ public class EcoreGLSPModule extends DefaultGLSPModule {
 	@Override
 	protected Class<? extends ToolPaletteItemProvider> bindToolPaletteItemProvider() {
 		return EcoreToolPaletteItemProvider.class;
+	}
+	
+	@Override
+	protected void configureClientActions(MultiBindConfig<Action> bindings) {
+		super.configureClientActions(bindings);
+		bindings.add(ReturnAttributeTypesAction.class);
 	}
 	
 	@Override
