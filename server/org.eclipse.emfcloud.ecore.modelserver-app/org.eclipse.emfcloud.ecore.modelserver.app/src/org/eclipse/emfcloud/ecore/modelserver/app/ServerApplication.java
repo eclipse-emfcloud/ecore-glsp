e3424d0cae3693371f39eb6f3f788e629b3a4dc2
Nina Doschek <ndoschek@eclipsesource.com>
2020-11-10 13:32:03 +0100

    Retrieve modelserver via P2 repositories
    Signed-off-by: Nina Doschek <ndoschek@eclipsesource.com>

---
+++ server/org.eclipse.emfcloud.ecore.modelserver-app/org.eclipse.emfcloud.ecore.modelserver.app/src/org/eclipse/emfcloud/ecore/modelserver/app/ServerApplication.java
@@ -0,0 +1,32 @@
+package org.eclipse.emfcloud.ecore.modelserver.app;
+
+import org.eclipse.emfcloud.ecore.modelserver.EcoreModelServerLauncher;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * An application to start the Ecore GLSP Modelserver.
+ */
+public class ServerApplication implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		String[] args = getArgs(context);
+		EcoreModelServerLauncher.main(args);	
+		return null;
+	}
+
+	private String[] getArgs(IApplicationContext context) {
+		Object object = context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+		if (object instanceof String[]) {
+			return (String[])object;
+		}
+		return new String[0];
+	}
+
+	@Override
+	public void stop() {
+		// Nothing
+	}
+
+}
