0d4ee22866efcd8e0be6e7c8d2225526da7925a2
Nina Doschek <ndoschek@eclipsesource.com>
2020-11-30 17:59:11 +0100

    Delete elements
    - Add support for removing elements
    - Make sure to remove existing eReferences with the removed EType
    - Extend empty.ecore for testing purposes
    
    Signed-off-by: Nina Doschek <ndoschek@eclipsesource.com>

--- server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/model/EcoreModelServerAccess.java
+++ server/org.eclipse.emfcloud.ecore.glsp/src/main/java/org/eclipse/emfcloud/ecore/glsp/model/EcoreModelServerAccess.java
@@ -10,20 +10,32 @@
  ********************************************************************************/
 package org.eclipse.emfcloud.ecore.glsp.model;
 
+import java.util.Collection;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EGenericType;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.util.EcoreUtil.UsageCrossReferencer;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.RemoveCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emfcloud.ecore.glsp.EcoreFacade;
 import org.eclipse.emfcloud.modelserver.client.ModelServerClient;
 import org.eclipse.emfcloud.modelserver.client.NotificationSubscriptionListener;
 import org.eclipse.emfcloud.modelserver.command.CCommand;
+import org.eclipse.emfcloud.modelserver.command.CCommandFactory;
+import org.eclipse.emfcloud.modelserver.command.CCompoundCommand;
+import org.eclipse.emfcloud.modelserver.command.CommandKind;
 import org.eclipse.emfcloud.modelserver.common.codecs.EncodingException;
 import org.eclipse.emfcloud.modelserver.edit.CommandCodec;
 import org.eclipse.glsp.server.model.GModelState;
@@ -84,23 +96,103 @@ public class EcoreModelServerAccess {
 	}
 
 	public boolean addEClassifier(EcoreModelState modelState, EClassifier newEClassifier) {
-		return this.add(modelState, newEClassifier, EcorePackage.eINSTANCE.getEPackage_EClassifiers());
+		return this.add(modelState, getEPackage(modelState), EcorePackage.Literals.EPACKAGE__ECLASSIFIERS,
+				newEClassifier);
 	}
 
-	private boolean add(EcoreModelState modelState, EObject newEClassifier, Object owner) {
-		EPackage ePackage = getEPackage(modelState);
+	private boolean add(EcoreModelState modelState, EObject owner, EReference feature, EObject element) {
 		Command addCommand = AddCommand.create(
-				EcoreModelState.getEditorContext(modelState).getResourceManager().getEditingDomain(), ePackage, owner,
-				newEClassifier);
+				EcoreModelState.getEditorContext(modelState).getResourceManager().getEditingDomain(), owner, feature,
+				element);
 		return this.edit(addCommand);
 	}
 
-	public boolean delete(EcoreModelState modelState, EObject element) {
-		EPackage ePackage = getEPackage(modelState);
-		Command removeCommand = RemoveCommand.create(
-				EcoreModelState.getEditorContext(modelState).getResourceManager().getEditingDomain(), ePackage,
-				EcorePackage.eINSTANCE.getEPackage_EClassifiers(), element);
-		return this.edit(removeCommand);
+	public boolean removeEReference(EcoreModelState modelState, EReference eReference) {
+		EReference eOpposite = eReference.getEOpposite();
+		EReference feature = EcorePackage.Literals.ECLASS__ESTRUCTURAL_FEATURES;
+		if (eOpposite != null) {
+			Command removeCommand = createRemoveCommand(modelState, eReference, feature);
+			Command removeOppositeCommand = createRemoveCommand(modelState, eOpposite, feature);
+			CCompoundCommand compoundCommand = CCommandFactory.eINSTANCE.createCompoundCommand();
+			compoundCommand.setType(CommandKind.COMPOUND);
+			try {
+				compoundCommand.getCommands().add(getCommandCodec().encode(removeCommand));
+				compoundCommand.getCommands().add(getCommandCodec().encode(removeOppositeCommand));
+			} catch (EncodingException e) {
+				return false;
+			}
+			return this.editCompound(compoundCommand);
+		}
+		return this.remove(modelState, eReference, feature);
+	}
+
+	public boolean removeEClassifier(EcoreModelState modelState, EClassifier eClassifier) {
+		Command removeEClassifierCommand = createRemoveCommand(modelState, eClassifier,
+				EcorePackage.Literals.EPACKAGE__ECLASSIFIERS);
+
+		Collection<Setting> usages = UsageCrossReferencer.find(eClassifier, eClassifier.eResource().getResourceSet());
+		if (!usages.isEmpty()) {
+			CCompoundCommand compoundCommand = CCommandFactory.eINSTANCE.createCompoundCommand();
+			compoundCommand.setType(CommandKind.COMPOUND);
+
+			try {
+				compoundCommand.getCommands().add(getCommandCodec().encode(removeEClassifierCommand));
+			} catch (EncodingException e) {
+				return false;
+			}
+
+			for (Setting setting : usages) {
+				EObject eObject = setting.getEObject();
+				if (setting.getEStructuralFeature().isChangeable() && eObject instanceof EStructuralFeature) {
+					Command removeCommand = createRemoveCommand(modelState, eObject,
+							EcorePackage.Literals.ECLASS__ESTRUCTURAL_FEATURES);
+					try {
+						compoundCommand.getCommands().add(getCommandCodec().encode(removeCommand));
+					} catch (EncodingException e) {
+						return false;
+					}
+				} else if (setting.getEStructuralFeature().isChangeable() && eObject instanceof EGenericType) {
+					Command removeCommand = createRemoveCommand(modelState, eObject,
+							EcorePackage.Literals.ECLASS__ESUPER_TYPES);
+					try {
+						compoundCommand.getCommands().add(getCommandCodec().encode(removeCommand));
+					} catch (EncodingException e) {
+						return false;
+					}
+				}
+			}
+			return this.editCompound(compoundCommand);
+		} else {
+			return this.edit(removeEClassifierCommand);
+		}
+	}
+
+	public boolean removeEAttribute(EcoreModelState modelState, EAttribute eAttribute) {
+		return this.remove(modelState, eAttribute, EcorePackage.Literals.ECLASS__ESTRUCTURAL_FEATURES);
+	}
+
+	public boolean removeEEnumLiteral(EcoreModelState modelState, EEnumLiteral eEnumLiteral) {
+		return this.remove(modelState, eEnumLiteral, EcorePackage.Literals.EENUM__ELITERALS);
+	}
+
+	private boolean remove(EcoreModelState modelState, EObject element, EReference feature) {
+		return this.edit(createRemoveCommand(modelState, element, feature));
+	}
+
+	private Command createRemoveCommand(EcoreModelState modelState, EObject element, EReference feature) {
+		EditingDomain editingDomain = EcoreModelState.getEditorContext(modelState).getResourceManager()
+				.getEditingDomain();
+		int index = element.eContainer().eContents().indexOf(element);
+		return RemoveCommand.create(editingDomain, element.eContainer(), feature, index);
+	}
+
+	private boolean editCompound(CCompoundCommand compoundCommand) {
+		try {
+			return this.modelServerClient.edit(modelUri, compoundCommand, FORMAT_XMI).thenApply(res -> res.body())
+					.get();
+		} catch (InterruptedException | ExecutionException e) {
+			return false;
+		}
 	}
 
 	private boolean edit(Command command) {
